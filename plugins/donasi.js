83ec4ee3dc26df7c4d0db103684941707be0ed78
SixxTenz <97493187+JulianKastaraaaa09@users.noreply.github.com>
2022-01-25 18:27:18 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -12,7 +12,7 @@ let handler = async (m, { conn, text }) => {
 *SAWERIA* : saweria.co/JulianKastara
 
 `.trim()
-await conn.sendButtonLoc(m.chat, await (await fetch(`https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Donasi`)).buffer(), ext.trim(), 'HanzzBotz By Hanzz-kun', '🙋‍♂️ NOMER OWNER', '.owner',m)
+await conn.sendButtonLoc(m.chat, await (await fetch(`https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Donasi`)).buffer(), ext.trim(), 'SixxTenz Ganteng', '🙋‍♂️ NOMER OWNER', '.owner',m)
     
 
 }
