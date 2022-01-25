31ee68e479f0dde23a0500fe0f211658dc509610
SixxTenz <97493187+JulianKastaraaaa09@users.noreply.github.com>
2022-01-25 18:24:15 +0700

    Update rulesyve.js

--- plugins/rulesyve.js
+++ plugins/rulesyve.js
@@ -1,77 +1,49 @@
-let { performance } = require('perf_hooks')
-let fs = require ('fs')
-let path = require('path')
-let handler  = async (m, { conn, usedPrefix }) => { 
-  let package = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json')))
-  let _uptime = process.uptime() * 1000
-  let uptime = clockString(_uptime) 
-  let totalreg = Object.keys(global.db.data.users).length
-  let old = Math.round(performance.now())
-  //await m.reply('wait Kakak!!')
-  let neww = Math.round(performance.now())
-  conn.reply(m.chat, `
-═〘 Rules ${namabot} 〙═
-
-⬡ Mohon untuk tidak Spam Bot karena Bot ini sudah memiliki Otomatis Blokir Bagi Pengguna Bot yang Spam
-
-⬡ Mohon maaf jika ad yg memakai menu Nsfw maka akan di block oleh bot, jika di gc tolong admin untuk menghubungi kami!!
-
-⬡ Mohon untuk tidak Menelpon Bot karena Bot ini sudah memiliki Otomatis Blokir Bagi orang yang nelpon Bot
-
-⬡ Kami tidak bertanggung jawab atas penyalahgunaan bot
-
-⬡ Kami tidak bertanggung jawab atas kebocoran data pribadi anda
+let fetch = require('node-fetch')
+let handler = async (m, { conn, text }) => {
+	await m.reply(' _*Baca Rulesnya Banh...*_ ')
+  let ext= `
+ ── 「 *PANDUAN* 」 ──
+1.User Dapat Menggunakan Semua Command Gratis Dengan Batasan Tiket
+Kecuali User Premium Dengan Tiket Unlimited/Tanpa Batas
+2.Saat Pertama Mendaftar User Akan Diberi 10 Tiket, Setiap 24 Jam User Akan Diberi 10 Tiket 
+Jika Tiket Anda Habis Anda Dapat Membelinya Dengan
+Command .buy <Jumlah Tiket> Contoh .buy 5
+1 Tiket = 350 Xp
+3.Pengguna Yang Mengirim Media Atau Data Pribadi Tidak Akan Disimpan Didalam Bot Ini
+Dan Kami Tidak Akan Bertanggung Jawab Atas Data Tersebut
+4.Jika Bot Tidak Membaca Pesan Anda Tandanya Bot Sedang Off
+Jika Bot Membaca Pesan Dan Tidak Merespon Tanda Nya Bot Sedang Delay Karena Gw Gak Pake Rdp
+5.Bot Ini Dilengkapi Dengan Fitur Antispam Jika Anda Spam 20 Pesan Tanpa Colldown 5 Detik Bot Ini Akan Otomatis Membanned Anda
+6.Jika Anda Melanggar Rules Maka Owner Akan Memberi Peringatan Kepada Anda
+7.Jika Anda Menelpon Bot Anda Akan Diberi Warn/Soft Block
+8.Jika Anda Adalah User Premium Lalu Anda Dibanned Karena Melanggar Rules, Owner Tidak Akan Menanggung Kerugian Anda
+9.Jika Ada Bug Dalam Fitur Bot Silahkan Laporkan Ke owner Dengan Command .report <Laporan Anda>
+10.Jika Ingin Request Fitur Baru Silahkan Ketik .request <Fitur Yang Ingin Direquest>
 
 
-➥ ${namabot} Versi ${package.version}
-➥ *HomePage:* https://youtu.be/d7dcw2CO4ew
-➥ *Issue:* https://youtu.be/d7dcw2CO4ew
-➥ *Ping:* ${neww - old} *ms*
-➥ *Total user:* ${totalreg} *user*
-➥ *Uptime:* ${uptime}
 
-═ 〘 DONASI 〙 ═
-➥ Saweria : https://saweria.co/raraharsita2
-➥ Dana: [0882-7926-8363]
+ ── 「 *RULES* 」 ──
+  1.DILARANG MERUSAK SERVER BOT ATAU MENGIRIMKAN VIRTEX YANG MEMBUAT BOT RUSAK
+  2.DILARANG MENELPON/MENVC BOT
+  3.DILARANG SPAM BOT
+  4.DILARANG MENGCOPY/MENYALIN PESAN DARI BOT
+`.trim()
+await conn.sendButtonLoc(m.chat, await (await fetch(`https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Panduan Dan Rules`)).buffer(), ext.trim(), 'SixxTenz Ganteng', '👤 NOMER OWNER', '.owner',m)
+    
 
-
-➥ Request? https://youtu.be/d7dcw2CO4ew
-
-Official Grup :
-
-Grup 1 :
-https://chat.whatsapp.com/HAZ6yFgCafUAeDbNH33IrL
-
-Grup 2 :
-https://chat.whatsapp.com/HAZ6yFgCafUAeDbNH33IrL
-
-Grup 3 :
-https://chat.whatsapp.com/HAZ6yFgCafUAeDbNH33IrL
-
-
- ©BaseNew ${namabot}
- Script original by King Of Bear
- 
- 
-═〘 ${namabot} 〙 ═
-`.trim(), m)
 }
+handler.help = ['rules']
+handler.tags = ['info']
+handler.command = /^rules/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
 
-handler.help = ['main']
-handler.tags = ['infobot', 'rules']
-handler.command = handler.command = /^(infobot|rules)$/i
+handler.admin = false
+handler.botAdmin = false
 
 handler.fail = null
 
 module.exports = handler
-
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
-
-function clockString(ms) {
-  let h = Math.floor(ms / 3600000)
-  let m = Math.floor(ms / 60000) % 60
-  let s = Math.floor(ms / 1000) % 60
-  console.log({ms,h,m,s})
-  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
-}
